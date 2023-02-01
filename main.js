7c6aaf3f19f2336d0a4bf624a5cc965b591b8237
Thomas Lopez <ricardo.t.villafranco@gmail.com>
2023-02-01 00:07:38 -0600

    Logica del juego funcionando

--- main.js
+++ main.js
@@ -4,7 +4,64 @@ pincel.fillStyle = "black";
 pincel.fillRect(0,0,600,400);
 var logoOneP = new Image();
 logoOneP.src = "./img/onepieceLogo.png";
+var radio = 60;
+var xRandom;
+var yRandom;
 
+
+function randomPosition(max) {
+    return Math.floor(Math.random()*max)
+}
+
+$(logoOneP).on("load",function() {
+    pincel.drawImage(logoOneP,xRandom, yRandom,100,100);
+});
+
+ xRandom = randomPosition(500);
+ yRandom = randomPosition(300);
+
+function clearScreen(){
+    pincel.clearRect(0,0,600,400);
+}
+function updateScreen(){
+    clearScreen();
+    xRandom = randomPosition(500);
+    yRandom = randomPosition(300);
+    printTarget(xRandom,yRandom)
+}
+function printTarget(x,y){
+        printCircle((x+50),(y+50),radio,"red");
+        pincel.drawImage(logoOneP,x,y,100,100);
+        
+}
+
+
+
+setInterval(updateScreen,1000)
+
+
+
+
+function shoot (evento) {
+   var x = evento.pageX - pantalla.offsetLeft;
+   var y = evento.pageY - pantalla.offsetTop;
+
+   if((x< (xRandom+50) + radio)&&
+      (x> (xRandom+50) - radio)&&
+      (y <(yRandom+50) + radio)&&
+      (y > (yRandom+50) - radio)) {
+          alert("Nice shoot")
+      }  
+}
+function printCircle(x,y,radio,color){
+    pincel.fillStyle = color;
+     pincel.beginPath();
+     pincel.arc(x,y,radio,0,2*Math.PI);
+      pincel.fill();
+   }
+  
+
+ 
 // var img = getElementById("target")
 
 // pincel.drawImage(img, 100, 100);
@@ -23,48 +80,44 @@ logoOneP.src = "./img/onepieceLogo.png";
 
 
 
-var logo = new Image(100, 100)
-logo.src = "./img/onepieceLogo.png"
+// var logo = new Image(100, 100)
+// logo.src = "./img/onepieceLogo.png"
 
 
 
- document.body.appendChild(logo);
-logo.onclick= itsWorking;
+//  document.body.appendChild(logo);
+// logo.onclick= itsWorking;
 
-logoOneP.addEventListener("load", function(){
-    pincel.drawImage(logoOneP,Math.floor(Math.random()*(600-100)), Math.floor(Math.random()*(400-100)),100,100);
-});
+// logoOneP.addEventListener("load", function(){
+//     pincel.drawImage(logoOneP,Math.floor(Math.random()*(600-100)), Math.floor(Math.random()*(400-100)),100,100);
+// });
 
-logoOneP.addEventListener('click', function(){
-    alert("funciona")
-})
-function itsWorking () {
-    alert("funciona");
-}
-
-
-
-
+// logoOneP.addEventListener('click', function(){
+//     alert("funciona")
+// })
+// function itsWorking () {
+//     alert("funciona");
+// }
 
 
 
 
 
 
-// var radio = 10;
-// var xRandom;
-// var yRandom;
 
-// function printCircle(x,y,radio,color){
-//     pincel.fillStyle = color;
-//     pincel.beginPath();
-//     pincel.arc(x,y,radio,0,2*Math.PI);
+
+
+
+
+
+//  function printCircle(x,y,radio,color){
+//   pincel.fillStyle = color;
+//    pincel.beginPath();
+//    pincel.arc(x,y,radio,0,2*Math.PI);
 //     pincel.fill();
-// }
+//  }
 
-// function clearScreen(){
-//     pincel.clearRect(0,0,600,400 )
-// }
+
 
 // var x = 0;
 
@@ -112,19 +165,8 @@ function itsWorking () {
 
 // setInterval(updateScreen,800) 
 
-// function shoot (evento) {
-//     var x = evento.pageX - pantalla.offsetLeft;
-//     var y = evento.pageY - pantalla.offsetTop;
 
-//     if((x< xRandom + radio)&&
-//         (x> xRandom - radio)&&
-//         (y < yRandom + radio)&&
-//         (y > yRandom - radio)) {
-//             alert("Nice shoot")
-//         }  
-// }
-
-// pantalla.onclick = shoot;
+ pantalla.onclick = shoot;
 
 // var logo =document.createElement("img");
 // logo.style.width = "80px";
